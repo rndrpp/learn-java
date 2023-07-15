09a92567f1fa4f83172a4761f9b64f740399cbab
Mochammad Rendra Putra Pratama <106427298+rndrpp@users.noreply.github.com>
2023-07-15 23:13:44 +0900

    IfStatement

--- Array.java
+++ Array.java
@@ -22,6 +22,8 @@ public class Array {
             "Phiphi", "Benjamin", "Shibe"
         };
 
+        namaNama[0] = null;
+
         int[] arrayInt = new int[]{
             1,2,3,4,5,6,7,8,9,10
         };
@@ -29,6 +31,19 @@ public class Array {
         long[] arrayLong = {
             10L, 20L, 30L
         };
+
+        arrayLong[0] = 0;
+
+        System.out.println(arrayLong.length);
+
+        String[][] members =  {
+            {"Phiphi", "Shibe"},
+            {"Philip", "Shipley"},
+            {"Phelps"}
+        };
+
+        System.out.println(members[0][1]);
+        System.out.println(members[1][0]);
     }
 
 }
